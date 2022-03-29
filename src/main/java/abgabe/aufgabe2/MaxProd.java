3732713d0b1d93b60fb2550338395de8b72dd013
Sergej Bakanow <s.bakanow@gmail.com>
2022-03-29 14:00:27 +0200

    Implemented Abgabe 2

--- src/main/java/abgabe/aufgabe2/MaxProd.java
+++ src/main/java/abgabe/aufgabe2/MaxProd.java
@@ -1,17 +1,34 @@
 package abgabe.aufgabe2;
 
-public class MaxProd {
+public final class MaxProd {
 
-  public static double maxProd(double [] inputs) {
-    double bestProd = 0.0;
-    double bufferProd = 1.0;
-    for (var input: inputs) {
-      bufferProd *= input;
-      if(bestProd < bufferProd) {
-        bestProd = bufferProd;
+  public static double maxProd(final double[] inputs) {
+    double scanMax = 1.0;
+    double bisMax = 1.0;
+    int indexEnd = 0;
+
+    for (int i = 0; i < inputs.length; ++i) {
+      scanMax *= inputs[i];
+
+      if (bisMax < scanMax) {
+        bisMax = scanMax;
+        indexEnd = i;
       }
     }
-    return bestProd;
+
+    int indexStart = indexEnd;
+    for (double i = 1.0; i < bisMax; --indexStart) {
+        i *= inputs[indexStart];
+      if (i >= bisMax) {
+        break;
+      }
+    }
+
+    for (int i = indexStart; i <= indexEnd; ++i) {
+      System.out.println(inputs[i]);
+    }
+    System.out.println(indexStart + " " + indexEnd);
+    return bisMax;
   }
 
   public static void main(String[] args) {
