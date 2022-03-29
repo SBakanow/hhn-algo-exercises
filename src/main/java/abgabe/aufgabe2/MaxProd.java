8e082170de25a4cb89e6d20440651ade3d63b982
Marvin Simon <simon.marvin.1@web.de>
2022-03-29 23:24:47 +0200

    chore: Addded a few test arrays

--- src/main/java/abgabe/aufgabe2/MaxProd.java
+++ src/main/java/abgabe/aufgabe2/MaxProd.java
@@ -2,12 +2,22 @@ package abgabe.aufgabe2;
 
 public class MaxProd {
 
-  public static double maxProd(double [] inputs) {
+  /*
+    Input: A[] = { 6, -3, -10, 0, 2 }
+    Output: 180  // The subarray is {6, -3, -10}
+
+    Input: A[] = {-1, -3, -10, 0, 60 }
+    Output: 60  // The subarray is {60}
+
+    Input: A[] = { -2, -3, 0, -2, -40 }
+    Output: 80  // The subarray is {-2, -40}
+   */
+  public static double maxProd(double[] inputs) {
     double bestProd = 0.0;
     double bufferProd = 1.0;
-    for (var input: inputs) {
+    for (var input : inputs) {
       bufferProd *= input;
-      if(bestProd < bufferProd) {
+      if (bestProd < bufferProd) {
         bestProd = bufferProd;
       }
     }
@@ -15,6 +25,6 @@ public class MaxProd {
   }
 
   public static void main(String[] args) {
-    System.out.println(maxProd(new double[] {1.0, -2.0, -2.0, 1.5}));
+    System.out.println(maxProd(new double[] {-1, -3, -10, 0, 60}));
   }
 }
